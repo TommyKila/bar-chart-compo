66361fc344849a4f5a3a4f12017d227ff4bd873b
Tommy Moncaco <103176871+TommyKila@users.noreply.github.com>
2022-09-24 12:15:02 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,127 +1,78 @@
-const confirmBtn = document.querySelector('.confirm-btn');
-const form = document.querySelector('form');
-const inputField = document.querySelectorAll('.input-field');
-const realtimeText = document.querySelectorAll('.realtime-text');
-const completeState = document.querySelector('.complete-state');
-const resetBtn = document.querySelector('.reset-btn');
-const numberInput = document.getElementById('card-number');
-const monthInput = document.getElementById('month');
-const yearInput = document.getElementById('year');
-const nameInput = document.getElementById('cardholder-name');
-const cvcInput = document.getElementById('cvc');
-let count = 0;
-let numberPattern = /^(?:[0-9]{4}) (?:[0-9]{4}) (?:[0-9]{4}) (?:[0-9]{4})$/;
+const chartContainer = document.querySelector('.chart-cols')
 
-const defTextArr = Array.from(realtimeText)
+let tempDate = new Date();
+let tempDay = tempDate.getDay();
 
-const defValue = defTextArr.reduce(function (acc, text) {
-    return acc.concat(text.textContent);
-}, []);
-
-resetBtn.addEventListener('click', function () {
-    inputField.forEach(function (field) {
-        field.value = "";
-    });
-    realtimeText.forEach(function (text, index) {
-        text.textContent = defValue[index];
-    });
-    form.style.display = "block";
-    completeState.classList.remove("complete-message");
-});
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    inputField.forEach(function (field) {
-        checkBlank(field);
-    });
-
-    if (!numberPattern.test(numberInput.value) && numberInput.value !== "") {
-        numberInput.parentElement.classList.add("wrong-format");
-    } else {
-        numberInput.parentElement.classList.remove("wrong-format");
-    }
-
-    if (monthInput.value > 12 || monthInput.value < 1) {
-        monthInput.parentElement.classList.add("wrong-month");
-    } else {
-        monthInput.parentElement.classList.remove("wrong-month");
-    }
-
-    if (cvcInput.value.length < 3 && cvcInput.value !== "") {
-        cvcInput.parentElement.classList.add("cvc-format");
-    } else {
-        cvcInput.parentElement.classList.remove("cvc-format");
-    }
-
-
-    inputField.forEach(function (field) {
-        const fieldParent = field.parentElement;
-        if (!fieldParent.classList.contains("wrong-format") && !fieldParent.classList.contains("blank-alert") &&
-            !fieldParent.classList.contains("wrong-month") && !fieldParent.classList.contains("cvc-format")){
-            count++;
-        }
-    });
-
-    if (count === 5) {
-        form.style.display = "none";
-        completeState.classList.add("complete-message");
-    }
-    count = 0;
-})
-
-function checkBlank(item) {
-    if (item.value === "") {
-        item.parentElement.classList.add("blank-alert");
-    } else {
-        item.parentElement.classList.remove("blank-alert");
-    }
+function getData(callback) {
+    fetch('./data.json')
+        .then(function (data) {
+            return data.json();
+        }).then(callback);
 }
 
-nameInput.addEventListener('input', function (e) { 
-    e.target.value = e.target.value.replace(/[^\a-z A-Z]/g, '');
-});
+Array.prototype.move = function (from, to) {
+    this.splice(to, 0, this.splice(from, 1)[0]);
+};
 
-numberInput.addEventListener('input', function (e) {
-    e.target.value = e.target.value.replace(/[^\d]/g, '').replace(/(.{4})/g, '$1 ').trim();
-});
+function displayBar() {
+    getData(function (data) {
+        let bar = data.map(function (data) {
+            return `<div>
+                <div class="bar" data-id="${data.amount}"><span class="info">$${data.amount}</span></div>
+                <p>${data.day}</p>
+            </div>`
+        }).join('');
 
-monthInput.addEventListener('input', function (e) {
-    e.target.value = e.target.value.replace(/[^\d]/g, '').trim();
-});
+        chartContainer.innerHTML = bar;
 
-yearInput.addEventListener('input', function (e) {
-    e.target.value = e.target.value.replace(/[^\d]/g, '').trim();
-});
+        const chartBar = [...document.querySelectorAll('.bar')];
 
-cvcInput.addEventListener('input', function (e) {
-    e.target.value = e.target.value.replace(/[^\d]/g, '').trim();
-});
+        chartBar.forEach(function (bar) {
+            bar.style.height = `${(bar.dataset.id / 100) * 250}px`;
+        });
 
+        dayHighlight(chartBar, data);
+        hoverInfo(chartBar);
+    });
+}
 
-inputField.forEach(function (field) {
-    field.addEventListener('input', function (e) {
-        const inputId = e.currentTarget.id;
-        const fieldValue = e.currentTarget.value;
-        realtimeText.forEach(function (value, index) {
-            const valueId = value.dataset.id;
-            if (valueId == inputId) {
-                if (inputId === "month" || inputId === "year") {
-                    if (fieldValue < 10 && fieldValue > 0) {
-                        value.textContent = `0${fieldValue}`;
-                    } else {
-                        value.textContent = fieldValue;
-                    }
-                } else {
-                    value.textContent = fieldValue;
-                }
-            }
-            if (value.textContent === "") {
-                value.textContent = defValue[index];
+function dayHighlight(bar, obj) {
+    obj.move(6, 0);
+    obj.forEach(function (obj, index) {
+        if (index === tempDay) {
+            bar.forEach(function (bar) {
+                if (bar.dataset.id == obj.amount) {
+                    bar.classList.add('date-highlight');
+                };
+            });
+        }
+    });
+}
+
+function hoverInfo(bar) {
+    bar.forEach(function (bar) {
+        bar.addEventListener('mouseover', function (e) {
+            const info = e.currentTarget.firstChild;
+            info.classList.add('visible');
+            if (!bar.classList.contains('date-highlight')) {
+                bar.classList.add('bar-opacity');
+            } else {
+                bar.classList.add('highlight-opacity');
             }
         });
     });
-});
+    bar.forEach(function (bar) {
+        bar.addEventListener('mouseout', function (e) {
+            const info = e.currentTarget.firstChild;
+            info.classList.remove('visible');
+            if (bar.classList.contains('date-highlight')) {
+                bar.classList.remove('highlight-opacity');
+            } else {
+                bar.classList.remove('bar-opacity');
+            }
+        });
+    });
+}
 
-
+window.addEventListener('DOMContentLoaded', displayBar())
 
